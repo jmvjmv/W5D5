738c4e76207d265a9524e02504b42188cb173348
jmvjmv <jmv8928@gmail.com>
2019-12-20 17:16:39 -0500

    Added some models

---
+++ Polls/polls_app/app/models/answer_choice.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: answer_choices
+#
+#  id          :bigint           not null, primary key
+#  text        :string           not null
+#  question_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class AnswerChoice < ApplicationRecord
+
+end
